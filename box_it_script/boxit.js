d2d5ea099473ab330714c066512075fc5ca42b7f
Selim OZDOGAN <selimozdogan@Selims-MacBook-Pro.local>
2020-03-21 00:20:49 -0700

    Finished Box It Script

--- box_it_script/boxit.js
+++ box_it_script/boxit.js
@@ -1,7 +1,4 @@
-let nameArr = process.argv.slice(2);
-console.log(boxIt(nameArr))
-
-function boxIt(nameArr) {
+const boxIt = nameArr => {
     let output = '';
     if (nameArr == undefined || nameArr.length < 1) {
         output += drawTopBorder(1) + '\n';
@@ -9,7 +6,7 @@ function boxIt(nameArr) {
         output += drawBottomBorder(1);
     }
     else {
-        let maxStringLength = getMaxLength(nameArr);
+        let maxStringLength = getlLengthofLongestString(nameArr);
         for (let i = 0; i < nameArr.length; i++) {
             if (i == 0)
                 output += drawTopBorder(maxStringLength) + '\n';
@@ -22,26 +19,12 @@ function boxIt(nameArr) {
     }
     return output;
 }
-function drawTopBorder(count) {
-    return '\u250F' + drawLine(count) + '\u2513';
-}
-function drawMiddleBorder(count) {
-    return '\u2523' + drawLine(count) + '\u252B';
-}
-function drawBottomBorder(count) {
-    return '\u2517' + drawLine(count) + '\u251B';
-}
-function drawLine(count) {
-    return '\u2501'.repeat(count);
-}
-function drawBarsAround(str) {
-    return `\u2503${str}\u2503`;
-}
-function getMaxLength(arr) {
-    let max = 0;
-    for (const value of arr) {
-        if (value.length > max)
-            max = value.length;
-    }
-    return max;
-}
+const drawTopBorder = count => '\u250F' + drawLine(count) + '\u2513';
+const drawMiddleBorder = count => '\u2523' + drawLine(count) + '\u252B';
+const drawBottomBorder = count => '\u2517' + drawLine(count) + '\u251B';
+const drawLine = count => '\u2501'.repeat(count);
+const drawBarsAround = str => `\u2503${str}\u2503`;
+const getlLengthofLongestString = arr => Math.max(...arr.map(x => x.length));
+
+let nameArr = process.argv.slice(2);
+console.log(boxIt(nameArr));
