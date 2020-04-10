f17f36a7582e05a043a80c2b52c80ea6ee27e65c
Selim OZDOGAN <selimozdogan@Selims-MacBook-Pro.local>
2020-04-09 21:41:07 -0700

    Team Picker is created and configured

---
+++ super_team_picker/knexfile.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      database: "team_picker",
+    },
+    migrations: {
+      tableName: "migrations",
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+};
