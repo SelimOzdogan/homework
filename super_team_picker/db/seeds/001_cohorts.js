06d575cc5cefe78127174aede9b44e83f3f225dc
Selim Ozdogan <selimozdogan@hotmail.com>
2020-04-12 16:59:01 -0700

    Designed Cohorts cards and added seeds

--- super_team_picker/db/seeds/001_cohorts.js
+++ super_team_picker/db/seeds/001_cohorts.js
@@ -1,13 +1,17 @@
+const faker = require("faker");
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex, Promise) {
+  return knex("cohorts")
+    .del()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
+      let index = 1;
+      const cohorts = Array.from({ length: 100 }).map(() => {
+        return {
+          name: `Cohort ${index++}`,
+          members: Array.from({ length: Math.floor(Math.random() * 30) + 5 }).map(() => faker.name.firstName()).join(", "),
+          logoUrl: faker.image.imageUrl()
+        };
+      });
+      return knex("cohorts").insert(cohorts);
     });
 };
