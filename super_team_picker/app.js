c9ad02607b845f33d303d905f11bfa0cd593c780
Selim Ozdogan <selimozdogan@hotmail.com>
2020-04-12 14:03:29 -0700

    added some changes

--- super_team_picker/app.js
+++ super_team_picker/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const logger = require("morgan");
 const app = express();
@@ -5,12 +6,15 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(logger("dev"));
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-
-
+const cohortsRouter = require("./routes/cohortsRouter");
+app.use("/cohorts", cohortsRouter);
 
 const PORT = 4545;
 const ADDRESS = "localhost";
---
+++ super_team_picker/routes/cohortsRouter.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const knex = require("../db/connection");
+
+const router = express.Router();
+
+router.get("/", (request, response) => {
+    knex("cohorts")
+        .orderBy("createdAt", "desc")
+        .then((cohorts) => {
+            response.render("cohorts/index", { cohorts });
+        });
+});
+
+router.post("/", (request, response) => {
+    const { name, members, logoUrl } = request.body;
+    knex("cohorts")
+      .insert({
+        name,
+        members,
+        logoUrl,
+      })
+      .returning("*")
+      .then((cohort) => {
+        response.redirect(`/cohorts/${cohort[0].id}`);
+      });
+  });
+router.get("/new", (req, res) => {
+    res.render("cohorts/new");
+});
+// router.get("/lists", (req, res) => {
+//     res.render("cohorts/lists");
+// });
+
+router.get("/edit", (req, res) => {
+    res.render("cohorts/edit");
+});
+
+router.get("/lists", (request, response) => {
+    knex("cohorts")
+        .orderBy("createdAt", "desc")
+        .then((cohorts) => {
+            response.render("cohorts/lists", { cohorts });
+        });
+});
+
+router.post("/cohorts", (request, response) => {
+    console.log(request.body);
+    const { name, members, logoUrl } = request.body;
+    knex("cohorts")
+        .insert({
+            name,
+            members,
+            logoUrl,
+        })
+        .returning("*")
+        .then((cohort) => {
+            response.redirect(`cohorts/${cohort[0].id}`);
+        });
+});
+
+router.get("/:id", (request, response) => {
+    const id = request.params.id;
+    knex("cohorts")
+        .where("id", id)
+        .first()
+        .then((cohort) => {
+            console.log(cohort);
+            if (cohort) {
+                response.render("cohorts/show", { cohort });
+            } else {
+                response.redirect("cohorts");
+            }
+        });
+});
+
+module.exports = router;
